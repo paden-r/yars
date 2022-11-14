7b941f95ed85cf820f1101ef0338652a7ec2a13a
Paden Rose <paden.rose@q2.com>
2022-11-14 11:56:06 -0600

    Correcting stored procedure

--- db_files/yars_GetPosts.sql
+++ db_files/yars_GetPosts.sql
@@ -1,7 +1,7 @@
 DROP PROCEDURE IF EXISTS yars.GetPosts;
 CREATE PROCEDURE yars.GetPosts()
 BEGIN
-    SELECT p.postid, p.createdate, p.title, p.BodyID
+    SELECT p.postid, p.createdate, p.title
     FROM posts p
     ORDER BY p.postid DESC;
 END;
