91bcdc1b82e7c4b1d902f99a9a1a0d14423d7a54
Paden Rose <rose.paden@gmail.com>
2022-11-20 11:32:15 -0600

    WIP: small refactor

--- db_files/yars_GetPosts.sql
+++ db_files/yars_GetPosts.sql
@@ -2,7 +2,7 @@ DROP PROCEDURE IF EXISTS yars.GetPosts;
 CREATE PROCEDURE yars.GetPosts()
 BEGIN
     SELECT p.postid, p.createdate, p.title
-    FROM posts p
+    FROM Posts p
     ORDER BY p.postid DESC;
 END;
 
